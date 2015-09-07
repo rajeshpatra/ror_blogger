fc6c9a75b25f45378077c2ced17b5fbe4619ab16
Rajesh Patra <p7.rajesh@gmail.com>
2015-09-08 02:05:24 +0530

    added destroy method along with delete option to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -12,9 +15,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new
-		@article.title = params[:article][:title]
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to article_path(@article)
 	end
+ 	
+ 	# You can use this method of article_params here or a new thing introduced using articles_helper.
+ 	# def article_params
+ 	# 	params.require(:article).permit(:title, :body)
+ 	# end
+
+ 	def destroy
+ 		@article = Article.find(params[:id])
+ 		@article.destroy
+ 		redirect_to article_path(@article)
+ 	end
+	
 end
